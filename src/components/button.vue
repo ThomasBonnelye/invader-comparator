d9bd56c1a5c2607b4851ef7042242c941100f2e8
Thomas Bonnélye <thomasbonnelye@mbp-de-thomas.home>
2025-08-03 21:30:16 +0200

    feat(button):: add structure

---
+++ src/components/button.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  action: {
+    type: Function,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <button @click="action">
+    {{ label }}
+  </button>
+</template>
