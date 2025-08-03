99902e1095ccfdc632625be31975a5285f18a446
ThomasBonnelye <bonnelye.thomas@gmail.com>
2025-08-03 21:48:49 +0200

    feat(searchbar): add structure

---
+++ src/components/searchbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: 'Rechercher...'
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function onInput(event) {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+  <input 
+    type="text" 
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="onInput"
+  />
+</template>
